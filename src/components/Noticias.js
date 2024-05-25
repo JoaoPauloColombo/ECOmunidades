b63e12ae0e810d4be3d8cd820b6c906a76465bb6
Ju4uz <joaosouzapaulo446@gmail.com>
2024-05-25 00:13:53 -0300

    Update Noticias.js

--- src/components/Noticias.js
+++ src/components/Noticias.js
@@ -3,12 +3,15 @@ import styled from 'styled-components';
 import noticiasImage1 from "../assetsFinal/noticia1.jpg";
 import noticiasImage2 from "../assetsFinal/noticia2.jpg";
 import noticiasImage3 from "../assetsFinal/noticia3.jpg";
+import { Link } from 'react-router-dom';
 
 const Section4Container = styled.section`
   height: 600px;
   display: flex;
   flex: 20 1 100%;
   background-color: white;
+  flex-direction: column;
+
   @media (max-width: 768px) {
     flex-direction: column;
 	height: 1000px;
@@ -95,6 +98,35 @@ const BoxNoticiasP = styled.p`
   }
 `;
 
+const Button = styled(Link)`
+  margin-top: 20px;
+  transition: all 0.3s ease-in-out;
+  box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.2);
+  padding-block: 1rem;
+  padding-inline: 1.25rem;
+  background-color: #136d58;
+  border-radius: 9999px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  color: #ffff;
+  gap: 10px;
+  font-weight: bold;
+  border: 3px solid #ffffff4d;
+  outline: none;
+  overflow: hidden;
+  font-size: 15px;
+  text-decoration: none;
+  align-self: center;
+`;
+
+const Icon = styled.svg`
+  fill: #ffffff;
+  height: 16px;
+  width: 16px;
+`;
+
 function Noticias() {
   return (
     <Section4Container>
@@ -120,6 +152,14 @@ function Noticias() {
           </BoxNoticias>
         </GroupNoticias>
       </Noticia>
+      <Button to="/noticias">
+            Saiba mais
+            <Icon viewBox="0 0 24 24" class="icon">
+              <path clip-rule="evenodd"
+                d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z"
+                fill-rule="evenodd"></path>
+            </Icon>
+      </Button>
     </Section4Container>
   );
 }
