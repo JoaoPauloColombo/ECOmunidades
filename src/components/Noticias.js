21fba705be7f660f4bb2b1ae394f452b50b614e1
Ju4uz <joaosouzapaulo446@gmail.com>
2024-05-25 00:17:26 -0300

    Update Noticias.js

--- src/components/Noticias.js
+++ src/components/Noticias.js
@@ -102,6 +102,7 @@ const Button = styled(Link)`
   transition: all 0.3s ease-in-out;
   box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.2);
   padding-block: 1rem;
+  top:5vh;
   padding-inline: 0.30rem;
   background-color: #136d58;
   border-radius: 9999px;
